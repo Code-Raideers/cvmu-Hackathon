0629c7e9647c6d4300b5f95f156c286cc01733ed
Nityam24 <nityampatel2402@gmail.com>
2023-03-16 13:24:44 +0530

    Searching and reume builder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Searching  from './Mycomponents/Searching';
+import Header from "./Mycomponents/Header/Header";
+import Body from "./Mycomponents/Body/Body";
+
+import "./App.css";
+
+export default function App(){
+  return(
+    <div className="App">
+    <Searching/>
+    {/* <Header />
+      <Body /> */}
+    </div>
+  );
+}
