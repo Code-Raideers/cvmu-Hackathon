7bac50b660b83c549a791284352fadfe9de26a9b
Nityam24 <nityampatel2402@gmail.com>
2023-03-16 14:16:18 +0530

    update the header image

--- src/Mycomponents/Header/Header.js
+++ src/Mycomponents/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-// import resumeSvg from "../../assets/resume.svg";
+import resumeSvg from "../../assets/resume.svg";
 
 import styles from "./Header.module.css";
 
@@ -15,9 +15,9 @@ function Header() {
           Make your own resume. <span>It's free</span>
         </p>
       </div>
-      {/* <div className={styles.right}>
+      <div className={styles.right}>
         <img src={resumeSvg} alt="Resume" />
-      </div> */}
+      </div>
     </div>
   );
 }
